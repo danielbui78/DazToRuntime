20c3a64f879a73c98b3b53b584fc9610e58a0389
danielbui78 <danielbui78@gmail.com>
2021-05-15 17:35:37 -0700

    Part 2: separate pathway for exporting animation specific file

--- Common/DzRuntimePluginAction.cpp
+++ Common/DzRuntimePluginAction.cpp
@@ -59,6 +59,8 @@ void UnofficialDzRuntimePluginAction::Export()
 
 	 if (Exporter)
 	 {
+		 bool bDoMaterials = false;
+
 		  DzFileIOSettings ExportOptions;
 		  ExportOptions.setBoolValue("doSelected", true);
 		  ExportOptions.setBoolValue("doVisible", false);
@@ -66,6 +68,7 @@ void UnofficialDzRuntimePluginAction::Export()
 		  {
 				ExportOptions.setBoolValue("doFigures", true);
 				ExportOptions.setBoolValue("doProps", true);
+				bDoMaterials = true;
 		  }
 		  else
 		  {
@@ -74,7 +77,7 @@ void UnofficialDzRuntimePluginAction::Export()
 		  }
 		  ExportOptions.setBoolValue("doLights", false);
 		  ExportOptions.setBoolValue("doCameras", false);
-		  ExportOptions.setBoolValue("doAnims", true);
+		  ExportOptions.setBoolValue("doAnims", false);
 		  if (AssetType == "SkeletalMesh" && ExportMorphs && MorphString != "")
 		  {
 				ExportOptions.setBoolValue("doMorphs", true);
@@ -89,39 +92,80 @@ void UnofficialDzRuntimePluginAction::Export()
 		  ExportOptions.setStringValue("format", FBXVersion);
 		  ExportOptions.setIntValue("RunSilent", !ShowFbxDialog);
 
-		  ExportOptions.setBoolValue("doEmbed", false);
-		  ExportOptions.setBoolValue("doCopyTextures", false);
-		  ExportOptions.setBoolValue("doDiffuseOpacity", false);
+		  ExportOptions.setBoolValue("doEmbed", true);
+		  ExportOptions.setBoolValue("doCopyTextures", true);
+		  ExportOptions.setBoolValue("doDiffuseOpacity", true);
 		  ExportOptions.setBoolValue("doMergeClothing", true);
 		  ExportOptions.setBoolValue("doStaticClothing", false);
 		  ExportOptions.setBoolValue("degradedSkinning", true);
 		  ExportOptions.setBoolValue("degradedScaling", true);
 		  ExportOptions.setBoolValue("doSubD", false);
 		  ExportOptions.setBoolValue("doCollapseUVTiles", false);
+		  ExportOptions.setBoolValue("doLocks", true);
+		  ExportOptions.setBoolValue("doLimits", true);
 
-		  // get the top level node for things like clothing so we don't get dupe material names
+		  ////////////
+		  // Prepare for Material exports (Skeleton and Static Mesh)
+		  ////////////
 		  DzNode* Parent = Selection;
-		  while (Parent->getNodeParent() != NULL)
-		  {
-				Parent = Parent->getNodeParent();
-		  }
-
-		  // rename duplicate material names
 		  QList<QString> MaterialNames;
 		  QMap<DzMaterial*, QString> OriginalMaterialNames;
-		  RenameDuplicateMaterials(Parent, MaterialNames, OriginalMaterialNames);
+		  if (bDoMaterials)
+		  {
+			  // get the top level node for things like clothing so we don't get dupe material names
+			  while (Parent->getNodeParent() != NULL)
+			  {
+				  Parent = Parent->getNodeParent();
+			  }
+			  // rename duplicate material names
+			  RenameDuplicateMaterials(Parent, MaterialNames, OriginalMaterialNames);
+		  }
 
+		  ///////////////
+		  // Prepare Animation exports
+		  ///////////////
+		  if (AssetType == "Animation")
+		  {
+			  // correct CharacterFolder
+			  CharacterFolder = ImportFolder + QDir::separator() + CharacterName.left(CharacterName.indexOf("@")) + QDir::separator();
+			  // correct animation filename
+			  CharacterFBX = CharacterFolder + CharacterName + ".fbx";
+			  // correct export options
+			  ExportOptions.setBoolValue("doAnims", true);
+			  // disable props, morphs, subD, textures, etc
+			  ExportOptions.setBoolValue("doEmbed", false);
+			  ExportOptions.setBoolValue("doCopyTextures", false);
+			  ExportOptions.setBoolValue("doDiffuseOpacity", false);
+			  ExportOptions.setBoolValue("doSubD", false);
+			  ExportOptions.setBoolValue("doCollapseUVTiles", false);
+			  ExportOptions.setBoolValue("doLocks", false);
+			  ExportOptions.setBoolValue("doLimits", false);
+		  }
+
+
+		  /////////////////////
+		  // START: Export FBX
+		  ///////////////////////
 		  QDir dir;
 		  dir.mkpath(CharacterFolder);
 
-		  SetExportOptions(ExportOptions);
+		  // Unnecessary, all code is already done above
+//		  SetExportOptions(ExportOptions);
+
 		  Exporter->writeFile(CharacterFBX, &ExportOptions);
 
 		  WriteConfiguration();
+		  ////////////////////////
+		  // DONE: Export FBX
+		  ////////////////////////
 
-		  // Change back material names
-		  UndoRenameDuplicateMaterials(Parent, MaterialNames, OriginalMaterialNames);
-	 }
+		  if (bDoMaterials)
+		  {
+			  // Change back material names
+			  UndoRenameDuplicateMaterials(Parent, MaterialNames, OriginalMaterialNames);
+		  }
+
+	 } // if (Exporter)
 }
 
 // If there are duplicate material names, save off the original and rename one
